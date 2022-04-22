53003cd88b1b87c2089d11ba0cc4a9d68442325e
Cihad Akkaya <akkayac84@gmail.com>
2022-04-22 12:09:02 -0400

    work on bug

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require ('express');
-
 require('dotenv').config();
 
 const app = express();
 
-const port = process.env.port
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/hi', (req, res) => {
+  console.log('hit')
   res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
